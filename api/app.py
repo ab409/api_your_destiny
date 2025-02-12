d0d4f27e4d66f73ed3d6b01b9f18509a3048319e
yuguangyuan <yuguangyuan@csc.com.cn>
2025-02-12 08:42:44 +0800

    创建api目前,尝试适配vercel
    Signed-off-by: yuguangyuan <yuguangyuan@csc.com.cn>

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@ import httpx
 from fastapi import FastAPI, Request, WebSocket
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from models import GithubUserModel
+from api.models import GithubUserModel
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
