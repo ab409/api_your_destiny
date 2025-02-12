6dc21f319da8300a9a9703653b7c13961321f81a
yuguangyuan <yuguangyuan@csc.com.cn>
2025-02-12 13:22:53 +0800

    增加websocket接口
    Signed-off-by: yuguangyuan <yuguangyuan@csc.com.cn>

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 # app.py
 import uvicorn
-from fastapi import FastAPI
-
-app = FastAPI()
-
+from fastapi import FastAPI, WebSocket
 from fastapi import APIRouter
 
+app = FastAPI()
 router = APIRouter()
 
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Message received: {data}")
+    except Exception as e:
+        print(f"WebSocket error: {e}")
+    finally:
+        await websocket.close()
 
 @router.get('/')
 async def get_user():
@@ -17,4 +26,4 @@ async def get_user():
 app.include_router(router)
 
 if __name__ == '__main__':
-	uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
